710942af3b7cea77182a4ea72f5bb2591df03d5a
Sulav Aryal <aryalpurna6@gmail.com>
2026-05-03 12:47:03 +0545

    Classes and object

---
+++ OOP/class_objecr.py
@@ -0,0 +1,15 @@
+# class Student:
+#     name= "Sulav Aryal"
+
+# s1= Student()
+# print(s1)
+# print(s1.name)
+
+
+class Cars:
+    color= "Red"
+    brand= "BMW"
+    Release_date= "2000"
+
+car1= Cars()
+print(car1.color, car1.brand, car1.Release_date)
