6a84d8e2276526874bd7dae228379b2bf7f671f0
Sulav Aryal <aryalpurna6@gmail.com>
2026-05-04 13:21:18 +0545

    Functions and recursion

---
+++ practise.py/function.py
@@ -0,0 +1,62 @@
+# def sum(a: int, b: int) -> int:
+#     return a+b
+
+# print(sum(3,5))
+
+
+
+
+
+# def avg():
+#     a=int(input("Enter number 1: "))
+#     b=int(input("Enter number 2: "))
+#     c=int(input("Enter number 3: "))
+
+#     average = (a+b+c)/3
+#     print(average)
+
+# avg()
+# avg()
+# avg()
+
+
+# def good_day(name, ending="Thank You"):
+#     print("Good Day," + name)
+#     print(ending)
+
+# good_day("Sulav", "Thanks You")
+# good_day("Prabin")
+
+
+
+# recursion
+# def factorial(n):
+#     if(n==0 or n==1):
+#         return 1
+#     return n * factorial(n-1)
+# n= int(input("Enter the number: "))
+# print( f"The Factorial of {n} is: {factorial(n)}") 
+
+
+# def greatest(a,b,c):
+#     if(a>b and a>c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     elif(c>a and c>b):
+#         return c
+    
+# a= int(input("Enter the value of a: "))
+# b= int(input("Enter the value of b: "))
+# c= int(input("Enter the value of c: "))
+
+# print( f"The Greates among a, b, c is : {greatest(a,b,c)}")
+
+
+# def pattern(n):
+#     if(n==0):
+#         return
+#     print("*" *n)
+#     pattern(n-1)
+
+# pattern(5)
