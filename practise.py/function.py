c3dc8bd36f34c08d8263ad4baeb3ce336456b8ea
Sulav Aryal <aryalpurna6@gmail.com>
2026-05-06 13:30:44 +0545

    functions

--- practise.py/function.py
+++ practise.py/function.py
@@ -60,3 +60,20 @@
 #     pattern(n-1)
 
 # pattern(5)
+
+
+# def hello(*name):
+#     print("Hello, My name is ", name[1])
+
+# hello("Sulav", "prabin", "safal")
+
+
+def fs(num):
+    if num == 1:
+        return (0)
+    elif num == 2:
+        return(1)
+    else:
+        return(fs(num-1) + fs(num-2))
+    
+print(fs(8))
